c3993cab6a621b3cd6e44b729c5e8b5a50f6692c
Cleve <gomescleve@gmail.com>
2015-11-23 09:57:23 +0400

    first commit

---
+++ application/controllers/Pages.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Developer1
+ * Date: 22/11/2015
+ * Time: 4:36 PM
+ */
+
+class Pages extends CI_Controller {
+
+	public function view($page = 'home')
+	{
+
+		if ( ! file_exists(APPPATH.'/views/pages/'.$page.'.php'))
+		{
+			// Whoops, we don't have a page for that!
+			show_404();
+		}
+
+		$data['title'] = ucfirst($page); // Capitalize the first letter
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('pages/'.$page, $data);
+		$this->load->view('templates/footer', $data);
+	}
+}
---
+++ application/views/news/success.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Developer1
+ * Date: 22/11/2015
+ * Time: 5:13 PM
+ */
+
+echo "<h2> Success </h2>";
